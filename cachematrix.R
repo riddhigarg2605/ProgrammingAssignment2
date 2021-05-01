f417e21d7c55fbcb454b55359bca9dc7d0810d71
RIDDHI <riddhi@RIDDHIs-MacBook-Pro.local>
2021-05-01 19:52:15 +0530

    commiting the first change

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+#Writing a pair of functions that cache the inverse of a matrix.
+
+## This function creates a special "matrix" object that can cache its own inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL #Setting the inverse to a null value, yet to be computed
+  s <- function(z) {
+    x <<- z ##Storing the x matrix outside of the current environment
+    inv <<- NULL #everytime there's a change, inv becomes null
+  }
+  g <- function() {x} ##Returns the x matrix
+  setsolve <- function(solve) {inv <<- solve} ##Storing the inverse outside of the current environment
+  getsolve <- function() {inv} ##Returns the inv that has been computed
+  list(s = s, g = g,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
